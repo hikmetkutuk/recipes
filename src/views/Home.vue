3057b8ec2eda8b39f4c45d3d9062a9a744e18271
Hikmet Kütük <hikmetkutuk@gmail.com>
2023-08-31 23:54:54 +0300

    created home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
-import store from '../store';
+import { computed } from "vue";
+import store from "../store";
 
-const meals = computed(() => store.state.meals)
+const meals = computed(() => store.state.meals);
+
+const letters = "ABCÇDEFGĞHIİJKLMNOÖQPRSŞTUÜVXYZ".split("");
 </script>
 
 <template>
-<pre>{{ meals }}</pre>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="8">
+        <v-text-field
+          clearable
+          rounded
+          variant="outlined"
+          label="Search for Meals"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="auto" v-for="letter of letters" :key="letter">
+        <router-link :to="{ name: 'byLetter', params: { letter } }">
+          {{ letter }}
+        </router-link>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
